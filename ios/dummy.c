a49519a46fac01de71f31bb7476742caafb10886
Lukas <lukutim@gmail.com>
2020-10-20 14:22:15 +0300

    linker flags

---
+++ ios/dummy.c
@@ -0,0 +1,9 @@
+//
+//  dummy.c
+//  Sound
+//
+//  Created by Lukas Malinauskas on 2020-10-20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#include "dummy.h"
---
+++ ios/dummy.h
@@ -0,0 +1,14 @@
+//
+//  dummy.h
+//  Sound
+//
+//  Created by Lukas Malinauskas on 2020-10-20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#ifndef dummy_h
+#define dummy_h
+
+#include <stdio.h>
+
+#endif /* dummy_h */
